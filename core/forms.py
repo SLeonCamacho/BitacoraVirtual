aab56c8b24033f641880f51565a1307412e3d578
Santiago Leon <sleoncamacho@outlook.com>
2023-06-27 11:51:24 -0500

    Proyecto iniciado

---
+++ core/forms.py
@@ -0,0 +1,8 @@
+from django.forms import forms
+from .models import Ingreso
+
+class IngresoFormulario(forms.ModelForm):
+    class Meta:
+        model=Ingreso
+
+        
---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Persona(models.Model):
+    nombre=models.CharField(max_length=20)
+    apellido=models.CharField(max_length=20)
+    cedula=models.CharField(max_length=10)
+
+    def cedula_valida(dni):
+        if len(dni)!=10:
+            raise Exception("Longitud incorrecta")
+        if int(dni[:2])<1 and int(dni[:2])>15:
+            raise Exception("Código de provincia incorrecto")
+        return True
+    
+
+class Ingreso(models.Model):
+    timestamp=models.DateTimeField()
+    motivo=models.TextField()
+    placa=models.CharField(blank=True, null=True, max_length=9)
+    persona=models.ForeignKey(Persona, on_delete=models.CASCADE)
